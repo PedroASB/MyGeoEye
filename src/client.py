c39616c30e2c28c55177f78598d348130ca1fc24
PedroASB <pedroasbelo@gmail.com>
2024-10-22 14:59:38 -0300

    feat: classes client e server
    Co-Authored-By: Daired <74380595+dairedalmeida@users.noreply.github.com>

--- src/client.py
+++ src/client.py
@@ -2,115 +2,127 @@ import socket
 import os
 from serialization import *
 
-FORMAT = 'utf-8'
 HOST_NAME = socket.gethostname()
 SERVER_IP = socket.gethostbyname(HOST_NAME)
-PORT = 5050
+PORT = 5000
 ADDRESS = (SERVER_IP, PORT)
-IMAGES_DIR = 'client_images'
 
-def upload_image(client_socket, directory, image_name):
-    """Envia uma imagem para o servidor"""
-    serialize_string(client_socket, image_name)
-    image_path = os.path.join(directory, image_name)
-    image_size = os.path.getsize(image_path)
-    serialize_int(client_socket, image_size)
-    with open(image_path, 'rb') as file:
-        while chunk := file.read(CHUNK_SIZE):
-            client_socket.send(chunk)
-    print(f'\nImagem "{image_name}" enviada com sucesso.')
+class Client:
+    IMAGES_DIR = 'client_images'
 
-
-def download_image(client_socket, directory, image_name):
-    """Baixa uma imagem do servidor"""
-    serialize_string(client_socket, image_name)
-    has_image = deserialize_bool(client_socket)
-    if not has_image:
-        print('\n[ERRO] Arquivo de imagem não encontrado.')
-        return
-    image_size = deserialize_int(client_socket)
-    image_path = os.path.join(directory, image_name)
-    with open(image_path, 'wb') as file:
-        received_size = 0
-        while received_size < image_size:
-            data = client_socket.recv(CHUNK_SIZE)
-            file.write(data)
-            received_size += len(data)
-    print(f'Imagem "{image_name}" armazenada com sucesso em "{directory}/".')
+    def __init__(self, ip=SERVER_IP, port=PORT):
+        self.ip = ip
+        self.port = port
+        self.address = (self.ip, self.port)
+        self.client_socket = None
+        if not os.path.exists(self.IMAGES_DIR):
+            os.makedirs(self.IMAGES_DIR)
 
 
-def list_images(client_socket):
-    """Lista as imagens que já foram salvas"""
-    num_images = deserialize_int(client_socket)
-    if num_images > 0:
-        images = deserialize_string(client_socket)
-        print('\nImagens armazenadas:\n' + images)
-    else:
-        print('\nNenhuma imagem armazenada.')
+    def upload_image(self, directory, image_name):
+        """Envia uma imagem para o servidor"""
+        serialize_string(self.client_socket, image_name)
+        image_path = os.path.join(directory, image_name)
+        image_size = os.path.getsize(image_path)
+        serialize_int(self.client_socket, image_size)
+        with open(image_path, 'rb') as file:
+            while chunk := file.read(CHUNK_SIZE):
+                self.client_socket.send(chunk)
+        print(f'\nImagem "{image_name}" enviada com sucesso.')
 
 
-def delete_image(client_socket, image_name):
-    """Deleta uma imagem"""
-    serialize_string(client_socket, image_name)
-    has_image = deserialize_bool(client_socket)
-    if has_image:
-        print(f'\nImagem "{image_name}" deletada com sucesso.')
-    else:
-        print('\n[ERRO] Arquivo de imagem não encontrado.')
+    def download_image(self, directory, image_name):
+        """Baixa uma imagem do servidor"""
+        serialize_string(self.client_socket, image_name)
+        has_image = deserialize_bool(self.client_socket)
+        if not has_image:
+            print('\n[ERRO] Arquivo de imagem não encontrado.')
+            return
+        image_size = deserialize_int(self.client_socket)
+        image_path = os.path.join(directory, image_name)
+        with open(image_path, 'wb') as file:
+            received_size = 0
+            while received_size < image_size:
+                data = self.client_socket.recv(CHUNK_SIZE)
+                file.write(data)
+                received_size += len(data)
+        print(f'Imagem "{image_name}" armazenada com sucesso em "{directory}/".')
 
 
-def start_client():
-    """Inicializa o cliente"""
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        client_socket.connect(ADDRESS)
-    except Exception:
-        print('[ERRO] Não foi possível estabelecer conexão com o servidor.')
-        exit(0)
-    print('[STATUS] Conexão com o servidor estabelecida.')
+    def list_images(self):
+        """Lista as imagens que já foram salvas"""
+        num_images = deserialize_int(self.client_socket)
+        if num_images > 0:
+            images = deserialize_string(self.client_socket)
+            print('\nImagens armazenadas:\n' + images)
+        else:
+            print('\nNenhuma imagem armazenada.')
 
-    while True:
-        print('\nSelecione um comando:')
-        print('1 - Enviar imagem')
-        print('2 - Baixar imagem')
-        print('3 - Listar imagens')
-        print('4 - Deletar imagem')
-        print('0 - Finalizar')
-        command = input('>> ')
-        match command:
-            case '1': # Inserir imagem
-                image_name = str(input('\nImagem a ser enviada: '))
-                image_path = os.path.join(IMAGES_DIR, image_name)
-                if not os.path.exists(image_path):
-                    print('\n[ERRO] Arquivo de imagem não encontrado.')
-                    continue
-                serialize_int(client_socket, 1)
-                upload_image(client_socket, IMAGES_DIR, image_name)
 
-            case '2': # Baixar imagem
-                serialize_int(client_socket, 2)
-                image_name = str(input('\nImagem a ser baixada: '))
-                download_image(client_socket, IMAGES_DIR, image_name)
+    def delete_image(self, image_name):
+        """Deleta uma imagem"""
+        serialize_string(self.client_socket, image_name)
+        has_image = deserialize_bool(self.client_socket)
+        if has_image:
+            print(f'\nImagem "{image_name}" deletada com sucesso.')
+        else:
+            print('\n[ERRO] Arquivo de imagem não encontrado.')
 
-            case '3': # Listar imagens
-                serialize_int(client_socket, 3)
-                list_images(client_socket)
 
-            case '4': # Deletar imagem
-                serialize_int(client_socket, 4)
-                image_name = str(input('\nImagem a ser deletada: '))
-                delete_image(client_socket, image_name)
+    def start(self):
+        """Inicializa o cliente"""
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.client_socket.connect(self.address)
+        except Exception:
+            print('[ERRO] Não foi possível estabelecer conexão com o servidor.')
+            exit(0)
+        print('[STATUS] Conexão com o servidor estabelecida.')
+        
+        """Inicializa o cliente"""
+        while True:
+            print('\nSelecione um comando:')
+            print('1 - Enviar imagem')
+            print('2 - Baixar imagem')
+            print('3 - Listar imagens')
+            print('4 - Deletar imagem')
+            print('0 - Finalizar')
+            command = input('>> ')
+            match command:
+                case '1': # Inserir imagem
+                    image_name = str(input('\nImagem a ser enviada: '))
+                    image_path = os.path.join(self.IMAGES_DIR, image_name)
+                    if not os.path.exists(image_path):
+                        print('\n[ERRO] Arquivo de imagem não encontrado.')
+                        continue
+                    serialize_int(self.client_socket, 1)
+                    self.upload_image(self.IMAGES_DIR, image_name)
 
-            case '0': # Encerrar conexão
-                serialize_int(client_socket, 0)
-                break
+                case '2': # Baixar imagem
+                    serialize_int(self.client_socket, 2)
+                    image_name = str(input('\nImagem a ser baixada: '))
+                    self.download_image(self.IMAGES_DIR, image_name)
 
-            case _:
-                print('\n[ERRO] Comando inválido.')
-    
-    client_socket.close()
-    print('\n[STATUS] Conexão com o servidor encerrada.')
+                case '3': # Listar imagens
+                    serialize_int(self.client_socket, 3)
+                    self.list_images()
+
+                case '4': # Deletar imagem
+                    serialize_int(self.client_socket, 4)
+                    image_name = str(input('\nImagem a ser deletada: '))
+                    self.delete_image(image_name)
+
+                case '0': # Encerrar conexão
+                    serialize_int(self.client_socket, 0)
+                    break
+
+                case _:
+                    print('\n[ERRO] Comando inválido.')
+        
+        self.client_socket.close()
+        print('\n[STATUS] Conexão com o servidor encerrada.')
 
 
 if __name__ == '__main__':
-    start_client()
+    client = Client()
+    client.start()
--- src/server.py
+++ src/server.py
@@ -4,105 +4,120 @@ import os
 from serialization import *
 
 HOST_NAME = socket.gethostname()
-SERVER_IP = socket.gethostbyname(HOST_NAME)
-PORT = 5050
-ADDRESS = (SERVER_IP, PORT)
-IMAGES_DIR = 'images_database'
+IP_SERVER = socket.gethostbyname(HOST_NAME)
+PORT_SERVER = 5000
+IP_CLUSTER = socket.gethostbyname(HOST_NAME)
+PORT_CLUSTER = 6000
+ADDRESS = (IP_SERVER, PORT_SERVER)
 
-def save_image(connection, directory):
-    """Armazena a imagem"""
-    image_name = deserialize_string(connection)
-    image_size = deserialize_int(connection)
-    image_path = os.path.join(directory, image_name)
-    with open(image_path, 'wb') as file:
-        received_size = 0
-        while received_size < image_size:
-            data = connection.recv(CHUNK_SIZE)
-            file.write(data)
-            received_size += len(data)
-    print(f'Imagem "{image_name}" armazenada com sucesso.')
+class Server:
+    IMAGES_DIR = 'images_database'
 
+    def __init__(self, ip=IP_SERVER, port=PORT_SERVER, cluster_ip=IP_CLUSTER, cluster_port=PORT_CLUSTER):
+        self.ip = ip
+        self.port = port
+        self.address = (self.ip, self.port)
+        self.cluster_ip = cluster_ip
+        self.cluster_port = cluster_port
+        if not os.path.exists(self.IMAGES_DIR):
+            os.makedirs(self.IMAGES_DIR)
+        
 
-def list_images(connection, directory):
-    """Lista todas as imagens que o cliente salvou"""
-    images = os.listdir(directory)
-    num_images = len(images)
-    serialize_int(connection, num_images)
-    if num_images > 0:
-        serialize_string(connection, '\n'.join(images))
-
-
-def send_image(connection, directory):
-    """Envia uma imagem para o cliente"""
-    image_name = deserialize_string(connection)
-    image_path = os.path.join(directory, image_name)
-    if not os.path.exists(image_path):
-        serialize_bool(connection, False)
-        return
-    serialize_bool(connection, True)
-    image_size = os.path.getsize(image_path)
-    serialize_int(connection, image_size)
-    with open(image_path, 'rb') as file:
-        while chunk := file.read(CHUNK_SIZE):
-            connection.send(chunk)
+    def save_image(self, connection, directory):
+        """Armazena a imagem"""
+        image_name = deserialize_string(connection)
+        image_size = deserialize_int(connection)
+        image_path = os.path.join(directory, image_name)
+        with open(image_path, 'wb') as file:
+            received_size = 0
+            while received_size < image_size:
+                data = connection.recv(CHUNK_SIZE)
+                file.write(data)
+                received_size += len(data)
+        print(f'Imagem "{image_name}" armazenada com sucesso.')
 
 
-def delete_image(connection, directory):
-    """Deleta uma imagem"""
-    image_name = deserialize_string(connection)
-    image_path = os.path.join(directory, image_name)
-    if not os.path.exists(image_path):
-        serialize_bool(connection, False)
-        return
-    serialize_bool(connection, True)
-    os.remove(image_path)
+    def list_images(self, connection, directory):
+        """Lista todas as imagens que o cliente salvou"""
+        images = os.listdir(directory)
+        num_images = len(images)
+        serialize_int(connection, num_images)
+        if num_images > 0:
+            serialize_string(connection, '\n'.join(images))
 
 
-def handle_client(connection, address):
-    """Gerencia a conexão com um cliente"""
-    print(f'[NOVA CONEXÃO] Cliente {address[0]}:{address[1]} conectado.')
-    while True:
-        option = deserialize_int(connection)
-        match option:
-            case 1: # Inserir imagem
-                print(f'[COMANDO] {address[0]}:{address[1]}: Inserir imagem.')
-                save_image(connection, IMAGES_DIR)
-                
-            case 2: # Baixar imagem
-                print(f'[COMANDO] {address[0]}:{address[1]}: Baixar imagem.')
-                send_image(connection, IMAGES_DIR)
+    def send_image(self, connection, directory):
+        """Envia uma imagem para o cliente"""
+        image_name = deserialize_string(connection)
+        image_path = os.path.join(directory, image_name)
+        if not os.path.exists(image_path):
+            serialize_bool(connection, False)
+            return
+        serialize_bool(connection, True)
+        image_size = os.path.getsize(image_path)
+        serialize_int(connection, image_size)
+        with open(image_path, 'rb') as file:
+            while chunk := file.read(CHUNK_SIZE):
+                connection.send(chunk)
 
-            case 3: # Listar imagens
-                print(f'[COMANDO] {address[0]}:{address[1]}: Listar imagens.')
-                list_images(connection, IMAGES_DIR)
+
+    def delete_image(self, connection, directory):
+        """Deleta uma imagem"""
+        image_name = deserialize_string(connection)
+        image_path = os.path.join(directory, image_name)
+        if not os.path.exists(image_path):
+            serialize_bool(connection, False)
+            return
+        serialize_bool(connection, True)
+        os.remove(image_path)
+
+
+    def handle_client(self, connection, address):
+        """Gerencia a conexão com um cliente"""
+        print(f'[NOVA CONEXÃO] Cliente {address[0]}:{address[1]} conectado.')
+        while True:
+            option = deserialize_int(connection)
+            match option:
+                case 1: # Inserir imagem
+                    print(f'[COMANDO] {address[0]}:{address[1]}: Inserir imagem.')
+                    self.save_image(connection, self.IMAGES_DIR)
                     
-            case 4: # Deletar imagem
-                print(f'[COMANDO] {address[0]}:{address[1]}: Deletar imagem.')
-                delete_image(connection, IMAGES_DIR)
+                case 2: # Baixar imagem
+                    print(f'[COMANDO] {address[0]}:{address[1]}: Baixar imagem.')
+                    self.send_image(connection, self.IMAGES_DIR)
 
-            case 0: # Encerrar conexão
-                print(f'[COMANDO] {address[0]}:{address[1]}: Encerrar conexão.')
-                break
-            
-            case _:
-                print(f'[ERRO] {address[0]}:{address[1]}: <comando inválido>')
+                case 3: # Listar imagens
+                    print(f'[COMANDO] {address[0]}:{address[1]}: Listar imagens.')
+                    self.list_images(connection, self.IMAGES_DIR)
+                        
+                case 4: # Deletar imagem
+                    print(f'[COMANDO] {address[0]}:{address[1]}: Deletar imagem.')
+                    self.delete_image(connection, self.IMAGES_DIR)
 
-    connection.close()
-    print(f'[CONEXÃO ENCERRADA] Cliente {address[0]}:{address[1]} desconectado.')
+                case 0: # Encerrar conexão
+                    print(f'[COMANDO] {address[0]}:{address[1]}: Encerrar conexão.')
+                    break
+                
+                case _:
+                    print(f'[ERRO] {address[0]}:{address[1]}: <comando inválido>')
+
+        connection.close()
+        print(f'[CONEXÃO ENCERRADA] Cliente {address[0]}:{address[1]} desconectado.')
 
 
-def start_server():
-    """Inicializa o servidor"""
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(ADDRESS)
-    server_socket.listen()
-    print(f'[STATUS] Servidor iniciado em {SERVER_IP}:{PORT}.')
-    while True:
-        connection, address = server_socket.accept()
-        thread = threading.Thread(target=handle_client, args=(connection, address))
-        thread.start()
-        print(f'Conexões ativas: {threading.active_count() - 1}')
+    def start(self):
+        """Inicializa o servidor"""
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(self.address)
+        server_socket.listen()
+        print(f'[STATUS] Servidor iniciado em {IP_SERVER}:{PORT_SERVER}.')
+        while True:
+            connection, address = server_socket.accept()
+            thread = threading.Thread(target=self.handle_client, args=(connection, address))
+            thread.start()
+            print(f'Conexões ativas: {threading.active_count() - 1}')
 
 
 if __name__ == '__main__':
-    start_server()
+    server = Server()
+    server.start()
